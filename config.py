4b027b82c2ca79758b82f785b522158012228c9c
dania-tii <dania.herzalla@tii.ae>
2025-01-27 13:46:10 +0400

    reset to gat model params

--- config.py
+++ config.py
@@ -1,23 +1,22 @@
 params = {
-    'model': 'GCN',
-    'learning_rate': 0.0004815321758405571,
+    'model': 'GAT',
+    'learning_rate': 0.0006703373871168542,
     'weight_decay': 0.00005,
-    'test_batch_size': 32,
-    'batch_size': 32,
+    'batch_size': 8,
     'dropout_rate': 0.5,
-    'num_heads': 8,
-    'num_layers': 2,
-    'hidden_channels': 512,
+    'num_heads': 4,
+    'num_layers': 8,
+    'hidden_channels': 256,
     'out_channels': 256,
     'out_features': 2,  # 3 jammer pos (x, y, z) # 5 (r, sin(theta), cos(theta), sin(phi), cos(phi))
-    'max_epochs': 200,
+    'max_epochs': 800,
     '3d': False,
     'coords': 'cartesian',  # opts: 'polar', 'cartesian'
     'required_features': ['node_positions', 'node_noise'],  # node_positions, polar_coordinates, node_noise, node_rssi
     'additional_features': ['mean_noise', 'std_noise', 'range_noise', 'dist_to_centroid', 'sin_azimuth', 'cos_azimuth'],
-    'num_neighbors': 10,
+    'num_neighbors': 'fc',
     'edges': 'knn',  # opts: 'knn', 'proximity'
-    'norm': 'unit_sphere',  # opts: 'minmax', 'unit_sphere'
+    'norm': 'minmax',  # opts: 'minmax', 'unit_sphere'
     'activation': False,
     'max_nodes': 400,
     'filtering_proportion': 0.6,
